75f87feacf6397988799d328469624868211787c
ib120 <106864556+ib120@users.noreply.github.com>
2022-08-26 10:08:37 +0530

    Add files via upload

---
+++ question 2.cpp
@@ -0,0 +1,39 @@
+/*ishan bansal
+2010990308*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void findRepeat(int* a, int n){
+    unordered_map<int,int> hash;
+    
+    for(int i=0;i<n;i++){
+        hash[a[i]]++;
+    }
+    
+    cout<<"repeated Number    frequency\n";
+   
+    for(auto it=hash.begin();it!=hash.end();it++)
+    if(it->second>1)
+    cout<<it->first<<"                  "<<it->second<<endl;
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"enter array length\n";
+    cin>>n;
+    int* a=(int*)(malloc(sizeof(int)*n));
+    
+    cout<<"input array elements...\n";
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    findRepeat(a,n);
+
+    return 0;
+}/*now print result*/
---
+++ question1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*ishan bansal
+201099308*
+non repeated numbers/
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        if(arr[i-1]!=arr[i] && arr[i]!=arr[i+1]){
+            cout<<arr[i]<<" ";
+        }/*now print result*/
+    }
+
+    return 0;
+}
